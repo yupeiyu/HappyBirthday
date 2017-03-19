0e7b09e2aefbc2a6f392867bf46271b27356ae6f
PeiYu Yu <omyupeiyu@gmail.com>
2017-03-19 21:20:45 +0800

    initial

---
+++ app/src/main/java/com/example/android/happybirthday/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.happybirthday;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+public class MainActivity extends ActionBarActivity {
+    int quantity=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
